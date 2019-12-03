4c31ddf618466ab2faa4c0b5a9975ad9d851b53b
Todd Ginsberg <todd@ginsberg.com>
2019-12-03 08:00:17 -0600

    Day 3 - Crossed Wires
    + Might refactor parseWire()

--- src/main/kotlin/com/ginsberg/advent2019/Points.kt
+++ src/main/kotlin/com/ginsberg/advent2019/Points.kt
@@ -10,10 +10,10 @@ data class Point2D(val x: Int, val y: Int) {
     fun distanceTo(other: Point2D): Int =
         abs(x - other.x) + abs(y - other.y)
 
-    val up: Point2D by lazy { Point2D(x, y + 1) }
-    val down: Point2D by lazy { Point2D(x, y - 1) }
-    val left: Point2D by lazy { Point2D(x - 1, y) }
-    val right: Point2D by lazy { Point2D(x + 1, y) }
+    val up: Point2D by lazy { copy(y = y + 1) }
+    val down: Point2D by lazy { copy(y = y - 1) }
+    val left: Point2D by lazy { copy(x = x - 1) }
+    val right: Point2D by lazy { copy(x = x + 1) }
 
     companion object {
         val ORIGIN = Point2D(0, 0)
